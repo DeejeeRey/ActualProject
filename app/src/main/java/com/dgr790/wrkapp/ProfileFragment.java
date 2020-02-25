c58a219c215f49b7d44536e2885e96f925201295
DeejeeRey <dgr790@student.bhama.ac.uk>
2020-02-25 16:00:09 +0000

    Profile Things

--- app/src/main/java/com/dgr790/wrkapp/ProfileFragment.java
+++ app/src/main/java/com/dgr790/wrkapp/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.dgr790.wrkapp;
 
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,6 +13,15 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -25,6 +35,14 @@ public class ProfileFragment extends Fragment {
     private TextView tvName, tvUser, tvEmail, tvPoints, tvTotal, tvAverage;
     private TextView tvPointsDisp, tvTotalDisp, tvAverageDisp;
     private ImageView ivUserInfo, ivUserStats;
+
+    private String firstname, lastname, username, email, score, times;
+    private String returnValueString;
+    private int returnValueInt;
+
+    private String userID;
+    private FirebaseUser currentUser;
+    private FirebaseAuth mAuth;
 
 
     @Nullable
@@ -46,8 +64,16 @@ public class ProfileFragment extends Fragment {
         ivUserInfo = (ImageView) v.findViewById(R.id.ivUserInfo);
         ivUserStats = (ImageView) v.findViewById(R.id.ivUserStats);
 
+        String[] dataNames = {"First Name", "Second Name", "Username", "Email", "Score", "Times"};
+
+
+
+        mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        userID = currentUser.getUid();
 
         setProfileImage();
+        setProfileInfo(dataNames);
 
         btnEdit.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -59,6 +85,59 @@ public class ProfileFragment extends Fragment {
 
 
         return v;
+    }
+
+    private void setProfileInfo(String[] dataNames) {
+        DatabaseReference currentDB = FirebaseDatabase.getInstance().getReference().child("Users").child(userID);
+        String[] dataValues = new String[6];
+
+        for (int i=0; i<4; i++) {
+            dataValues[i] = getStringData(dataNames[i], currentDB);
+        }
+
+        for (int i=4; i<6; i++) {
+            dataValues[i] = getIntData(dataNames[i], currentDB);
+        }
+
+        tvName.setText(dataValues[0] + dataValues[1]);
+        tvUser.setText(dataValues[2]);
+        tvEmail.setText(dataValues[3]);
+        tvPoints.setText(dataValues[4]);
+
+    }
+
+    private String getStringData(String type, DatabaseReference currentDB) {
+
+        currentDB.child(type).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                returnValueString = (String) dataSnapshot.getValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return returnValueString;
+    }
+
+    private String getIntData(String type, DatabaseReference currentDB) {
+
+        currentDB.child(type).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                returnValueInt = ((Long) dataSnapshot.getValue()).intValue();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+        return Integer.toString(returnValueInt);
     }
 
     private void changeLayout() {
@@ -75,6 +154,6 @@ public class ProfileFragment extends Fragment {
     }
 
     private void setProfileImage() {
-
+        Glide.with(this).load(currentUser.getPhotoUrl()).into(ivProfileImage);
     }
 }
